9d247b5b0d79c0e7ced736cfad2bebb4e1e0f79e
igrshrn <igrshrn@yandex.ru>
2025-05-14 17:49:08 +0300

    Homework#12-fix#3

--- src/test/java/ru/yandex/practicum/filmorate/controller/AbstractControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/AbstractControllerTest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
