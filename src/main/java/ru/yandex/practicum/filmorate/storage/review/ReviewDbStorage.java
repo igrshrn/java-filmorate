5a1c0937b1f387285ce6e6eb2d7c0983e3ae322d
igrshrn <igrshrn@yandex.ru>
2025-06-10 19:00:48 +0300

    Fix#3. Добавил сортировку по полезности отзывов

--- src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewDbStorage.java
@@ -20,8 +20,8 @@ public class ReviewDbStorage extends BaseRepository<Review> implements ReviewSto
     private static final String INSERT = "INSERT INTO reviews (content, is_positive, user_id, film_id, useful) VALUES (?, ?, ?, ?, ?)";
     private static final String UPDATE = "UPDATE reviews SET content = ?, is_positive = ?, user_id = ?, film_id = ?, useful = ? WHERE review_id = ?";
     private static final String FIND_BY_ID = "SELECT * FROM reviews WHERE review_id = ?";
-    private static final String FIND_ALL = "SELECT * FROM reviews LIMIT ?";
-    private static final String FIND_BY_FILM_ID = "SELECT * FROM reviews WHERE film_id = ? LIMIT ?";
+    private static final String FIND_ALL = "SELECT * FROM reviews LIMIT ? ORDER BY useful DESC";
+    private static final String FIND_BY_FILM_ID = "SELECT * FROM reviews WHERE film_id = ? ORDER BY useful DESC LIMIT ? ";
     private static final String DELETE = "DELETE FROM reviews WHERE review_id = ?";
 
     private static final String CHECK_VOTE = "SELECT is_like FROM review_votes WHERE review_id = ? AND user_id = ?";
