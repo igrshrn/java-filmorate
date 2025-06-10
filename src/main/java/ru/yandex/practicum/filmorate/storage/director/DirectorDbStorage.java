50b0d00e402ba68a70a0e6179fbb91c68b7572fc
ErmolaevSD <weekendarti@yandex.ru>
2025-06-10 14:41:56 +0300

    Fix_CheckStyle#1

--- src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorDbStorage.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 
 @Slf4j
 @Repository
-public class DirectorDbStorage extends BaseRepository<Director> implements DirectorStorage{
+public class DirectorDbStorage extends BaseRepository<Director> implements DirectorStorage {
 
     private static final String FIND_ALL_QUERY = "SELECT * FROM directors";
     private static final String FIND_BY_ID_QUERY = "SELECT * FROM directors WHERE id = ?";
